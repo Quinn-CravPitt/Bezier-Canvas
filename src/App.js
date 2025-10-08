5e9db9d0ec69fed8f9bc9a67102ad78931b01e7e
QuinnCP <cravenpq@oregonstate.edu>
2025-10-08 13:17:14 -0700

    add vid

--- src/App.js
+++ src/App.js
@@ -4,5 +4,20 @@ import BezierCanvas from "./BezierCanvas";
 function App() {
   return <BezierCanvas />;
 }
+<div style={{ marginTop: 20, textAlign: "center" }}>
+  <h3>How to Use This App</h3>
+  <div style={{ maxWidth: 560, margin: "0 auto" }}>
+    <iframe
+      width="100%"
+      height="315"
+      src="https://www.youtube.com/embed/MpOIOfbsSao?si=CZrekRUTKNT578QM"
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerPolicy="strict-origin-when-cross-origin"
+      allowFullScreen
+    ></iframe>
+  </div>
+</div>
 
 export default App;
