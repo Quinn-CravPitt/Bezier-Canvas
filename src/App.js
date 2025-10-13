5f7901dce26dbc6f2f88c7739491b75b358108a4
QuinnCP <cravenpq@oregonstate.edu>
2025-10-13 11:01:59 -0700

    afeafea

--- src/App.js
+++ src/App.js
@@ -16,20 +16,7 @@ function App() {
           flexWrap: "wrap", // allows stacking on smaller screens
         }}
       >
-        {/* Video section */}
-        <div style={{ flex: "0 0 560px" }}>
-          <h3 style={{ textAlign: "center" }}>How to Use This App</h3>
-          <iframe
-            width="100%"
-            height="315"
-            src="https://www.youtube.com/embed/MpOIOfbsSao?si=CZrekRUTKNT578QM"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerPolicy="strict-origin-when-cross-origin"
-            allowFullScreen
-          ></iframe>
-        </div>
+
 
         {/* Canvas section */}
         <div>
